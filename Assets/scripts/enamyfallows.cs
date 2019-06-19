f19b7583f00d023c8281cec63c1ffccb40f7c6bf
Mihai Vasilache <mihai.vasilache@gmail.com>
2019-06-19 19:50:39 +0300

    rigidbody

--- Assets/scripts/enamyfallows.cs
+++ Assets/scripts/enamyfallows.cs
@@ -7,15 +7,18 @@ public class enamyfallows : MonoBehaviour
     public float speed;
 
     private Transform target;
+    public Rigidbody2D rb2D;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed *Time.deltaTime);  
+        //rb2D.MovePosition(rb2D.position + Vector2.left * (Time.fixedDeltaTime * speed));
     }
 }
